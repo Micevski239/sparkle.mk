6c16cd0f4b7a85ad913faa0a1d50e492c24f9def
Micevski239 <filipmicevski04@gmail.com>
2026-01-19 21:19:10 +0100

    footer changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -300,13 +300,7 @@ export default function Layout() {
             <p className="text-xs text-white/60">
               © {new Date().getFullYear()} sparkle.mk. {t.footer.rights}
             </p>
-            <p className="text-xs text-white/60 flex items-center gap-1">
-              {t.footer.madeWith}{' '}
-              <svg className="w-3 h-3 text-red-400" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-              </svg>
-              {' '}{language === 'mk' ? 'во Македонија' : 'in Macedonia'}
-            </p>
+          
           </div>
         </div>
       </footer>
